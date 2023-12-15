e4820475e89c17d15a56fb6fcdf202368c4564cc
elmage78 <elmagesmm@gmail.com>
2023-12-15 22:17:04 +0100

    added lfs to local

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import matplotlib.pyplot as plt
+import yolov5
+import torch
 
+# load pretrained model
+model = yolov5.load('yolov5s.pt')
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 1000  # maximum number of detections per image
+model.to(torch.device(0))  # i.e. device=torch.device(0)
 
-cam = cv.VideoCapture(0)
-result, image = cam.read()
-
-cap = cv.VideoCapture(0)
-
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+cam = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    frame = cv.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
-    cv.imshow('Input', frame)
+    
+    result, image = cam.read()
+    # Capture frame-by-frame 
+    # inference with larger input size
+    results = model(image)
+    # parse results
+    #predictions = results.pred[0]
+    #boxes = predictions[:, :4] # x1, y1, x2, y2
+    #scores = predictions[:, 4]
+    #categories = predictions[:, 5]
+    Final = np.squeeze(results.render())
+    if result == True:
+       
+       cv2.namedWindow("Frame", cv2.WINDOW_AUTOSIZE) 
+       cv2.imshow('Frame',Final)
 
-    c = cv.waitKey(1)
-    if c == 27:
+        # Press Q on keyboard to  exit
+       if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-
-cap.release()
-cv.destroyAllWindows()
+    else: 
+        break
+ 
+# When everything done, release the video capture object
+cam.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
