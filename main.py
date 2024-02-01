5cb7c45b9769b60b95b34b356c54adea19a2173c
elmage78 <elmagesmm@gmail.com>
2024-02-01 18:51:24 +0100

    "tecnopole WORKING"

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ model.multi_label = False  # NMS multiple labels per box
 model.max_det = 1000  # maximum number of detections per image
 model.cpu  # i.e. device=torch.device(0)
 
-cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) 
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 People = []
 PeopleAmountInit = 0
+lastImgs = []
+Ticks = 0
 
 class Box:
     def __init__(self,x1,y1,x2,y2):
@@ -46,15 +50,13 @@ class Person:
        Message = str(self.name) + " is located in the box made of the points " + str(self.box.Pt1) + " and " + str(self.box.Pt2)
        return str(Message)
    
-
 def Capture():
+   lastImgs = []
+   Ticks = 0   
    result, image = cam.read()
-   h, w, c = image.shape
-
-   results = model(image, size=1980)
-   Predictions = results.pred[0] # 0 because it is image 1
+   results = model(image,size=1280)
+   Predictions = results.xyxy[0] # 0 because it is image 1
    NewPredictions = []
-
    for Prediction in Predictions:
         if Prediction[-1] == 0:
             NewPredictions.append(Prediction)
@@ -74,26 +76,64 @@ def Capture():
             print("error")
        People.append(Person("Joe " + str(Count),box))
    print(People[0])
+   
 
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640,640))
 Capture()
 while True:
-
+    Ticks += 1
     result, image = cam.read()
-    results = model(image)
+    images = []
+    heatmaps = []
+    tmp = []
+    fig, ax = plt.subplots()
+
+    for Pers in People:
+        images.append(image[int(Pers.box.y1):int(Pers.box.y2),int(Pers.box.x1):int(Pers.box.x2)])
+
+    i = 0
+    if Ticks > 1:
+        for img in images:
+            grisNow = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            grisBefore = cv2.cvtColor(lastImgs[i], cv2.COLOR_BGR2GRAY)
+
+            restados = cv2.absdiff(grisBefore, grisNow)
+
+            umbral = cv2.threshold(restados, 35, 255, cv2.THRESH_BINARY)[1]
+            contours, _ = cv2.findContours(umbral, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                # Get the bounding box of the contour
+                x, y, w, h = cv2.boundingRect(contour)
+
+                # If the contour is too small, skip it
+                if w < 100 or h < 100:
+                    continue
+                
+                # Draw a rectangle around the contour
+                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+                # Plot the movement on the map
+                ax.plot(x, y, 'ro')
+
+            heatmaps.append(img)
+            tmp.append(umbral)
+            i += 1
+    j = 0
     
-    Final = np.squeeze(results.render())
-    
-    if result == True:
-        out.write(Final)
-        cv2.imshow('frame', Final)
-        c = cv2.waitKey(1)
-        if c & 0xFF == ord('q'): break
-    else: 
+    for htmp in heatmaps:
+        cv2.imshow('Frame' + str(j),cv2.flip(htmp,1))
+        cv2.imshow('Frame*' + str(j),cv2.flip(tmp[j],1))
+        plt.pause(0.02)
+        if not result:
+            break
+        j += 1
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
+    if cv2.waitKey(10) & 0xFF == ord('y'):
+        lastImgs = images
+        Capture()
  
 
-
+plt.close()
 cam.release()
 cv2.destroyAllWindows()
