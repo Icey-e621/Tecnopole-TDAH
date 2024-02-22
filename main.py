e604caa8f9ac87d4ce022059421eccfdf47ed86d
Tecnopole <lcadornigap@gmail.com>
2024-02-22 18:56:05 +0100

    Actalizar

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from os import error
 import numpy as np
 import cv2
-import cv2
 import matplotlib.pyplot as plt
 from sympy import frac, true
 import yolov5
@@ -53,9 +52,12 @@ class Person:
        Message = str(self.name) + " is located in the box made of the points " + str(self.box.Pt1) + " and " + str(self.box.Pt2)
        return str(Message)
    
+TimesCalled = 0
 def Capture():
 #   lastImgs = []
-#   Ticks = 0   
+#   Ticks = 0 
+   global TimesCalled
+   TimesCalled = 1 + TimesCalled
    result, image = cam.read()
    results = model(image,size=1280)
    Predictions = results.xyxy[0] # 0 because it is image 1
@@ -70,17 +72,19 @@ def Capture():
        box = Box(Prediction[0],Prediction[1],Prediction[2],Prediction[3])
        box.Amplify(10)
        Count += 1
-       try:
-           if Person("Joe " + str(Count),box) == Person[Count-1]:
-              break
-           
-           elif Person("Joe " + str(Count),box) != Person[Count-1]:
-              People.insert(Count,Person("Joe " + str(Count),box) == Person[Count-1])
-       except error:
-            print(error)
-       People.append(Person("Joe " + str(Count),box))
-   print(People[0])
-   
+       if TimesCalled == 1:
+           People.append(Person("Joe " + str(Count),box))
+       else:
+            try:
+                if not People or Person("Joe " + str(Count),box) == People[Count-1]:
+                   People.append(Person("Joe " + str(Count),box))
+
+                elif Person("Joe " + str(Count),box) != People[Count-1]:
+                   print("t")
+                   People.insert(Count,Person("Joe " + str(Count),box) == People[Count-1])
+
+            except error:
+                 print(error)
 
 Capture()
 while True:
@@ -92,11 +96,13 @@ while True:
     fig, ax = plt.subplots()
 
     for Pers in People:
+        print("here now")
         images.append(image[int(Pers.box.y1):int(Pers.box.y2),int(Pers.box.x1):int(Pers.box.x2)])
         
     i = 0
     if Ticks > 1:
         for img in images:
+            print("here")
             grisNow = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             grisBefore = cv2.cvtColor(lastImgs[i], cv2.COLOR_BGR2GRAY)
@@ -122,27 +128,30 @@ while True:
             heatmaps.append(img)
             tmp.append(umbral)
             i += 1
-    j = 0
-    Pair = []
-    for htmp in heatmaps:
-        Pair.append(np.concatenate((cv2.flip(htmp,1),cv2.flip(tmp[j],1)), axis=0)) #1 = movement map, 2 = normal
-        plt.pause(0.02)
-        j += 1
-    
-    Last = 0
-    Now = 0
-    w = 0
-    for i in Pair:
-        if w == 0:
-            Last = i
+        j = 0
+        Pair = []
+        for htmp in heatmaps:
+            Cimage = cv2.hconcat([cv2.flip(htmp,1),cv2.flip(tmp[j],1)])
+            Pair.append(Cimage) #1 = movement map, 2 = normal (Concatenated horizontally)
+            plt.pause(0.02)
+            j += 1
+
+        Last = 0
+        Now = 0
+        w = 0
+        for tuples in Pair:
+            if w == 0:
+                Last = tuples
+                break
+            Last = cv2.vconcat([Last, tuples])
+            w += 1
+
+        while (result == true):
+           # cv2.imshow("frame", Last)
+            cv2.imshow("frame", cv2.hconcat([heatmaps[0],tmp[0]]))
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-        Last = np.concatenate((i,Last), axis = 1)
-        w += 1
-    
-    while (result == true):
-        cv2.imshow("frame", Last)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
     lastImgs = images
  
 
